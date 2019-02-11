870d23adb05e3a58527b0bc34988b9963597d342
Manoj Kumar Cebol Sundarrajan <mcebolsu@br018.pvt.bridges.psc.edu>
2019-02-11 00:03:37 -0500

    working on reading the ppm file

--- programming_assignment_1/convolution_2D.cpp
+++ programming_assignment_1/convolution_2D.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -12,7 +14,16 @@ int main(int argc, char* argv[])
 	}
 	string filename = argv[1];
 	int sigma = atoi(argv[2]);
-	
 
+	ifstream file(filename.c_str(), ios::binary);
+	string type;
+	int height,width,range;
+
+	file >> type >> height >> width >> range;
+
+
+	cout << type << endl << height << endl << width << endl << range << endl; 
+
+	file.close();
 	return 0;
 }
---
+++ programming_assignment_1/ppmcreator.c
@@ -0,0 +1,13 @@
+/* blueSquare.c */ 
+
+#include<stdio.h>
+
+int main(void) {
+int i;
+// this is the header of the ppm file
+ printf("P6\n 20 20 255\n"); // width = 400, height = 400
+// image data: loops over all the pixels and sets them all to bright yellow 
+for (i = 0; i < 20 * 20; i++) {
+printf("%c%c%c", 0, 0, 255); // r = 0, g = 0, b = 255 
+}
+return 0; }
