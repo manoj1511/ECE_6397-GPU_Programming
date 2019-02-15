9f28326241893900bd7123d839eeda4f51d41630
Manoj Kumar Cebol Sundarrajan <mcebolsundarrajan@uh.edu>
2019-02-15 17:19:01 -0500

    reads the comment lines and disposes it

--- programming_assignment_1/convolution_2D.cpp
+++ programming_assignment_1/convolution_2D.cpp
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-struct Pixel {unsigned char r, g, b; };
+struct Pixel {float r, g, b; };
 
 int main(int argc, char* argv[])
 {
-	if(argc !=3)
+	if(argc !=3)									//Checking if there are 3 arguments
 	{
-		cout<<"Please enter 3 arguments";
-		cout<<"USAGE:./executable image_file_name.ppm kernal_size ";
+		cout<<"Please enter 3 arguments";					//returns this message if 3 arguments not present
+		cout<<"USAGE:./executable image_file_name.ppm kernal_size ";		
 		return 1;
 	}
 
@@ -20,27 +20,46 @@ int main(int argc, char* argv[])
 	int sigma = atoi(argv[2]);
 
 	ifstream file(filename.c_str(), ios::binary);
+
 	string type;
+
+	string comment;
+	string dustbin;
+
+	file >> type; 
+	file >> comment;								// comment variable to store comments
+
+
+	while(comment.compare(0,1,"#") == 0)
+	{
+		getline(file,dustbin);
+		file >> comment;
+	}
+
 	int height,width,range;
 
-	file >> type >> height >> width >> range;
+	height = stoi(comment);
+	file >> width >> range;
 
 	int N = height*width;	
 
  	size_t pixel_size = height * width * sizeof(Pixel);	
 	
 	cout<< "size to be allocated 	: " << pixel_size << endl;
-	cout << type << endl << height << endl << width << endl << range << endl; 
+	cout << type << endl << height << endl << width << endl << range << endl; 	
 
-	Pixel *pixel = new Pixel[N];	
+	Pixel *pixel = new Pixel[N];							// allocate array to store pixel values
+
+	unsigned char temp;								// temp reg to store pixel values
 	
 	for(int i=0; i<N; i++)
 	{
-		file >> pixel[i].r; cout << (int)pixel[i].r << " ";
-		file >> pixel[i].g; cout << (int)pixel[i].g << " ";
-		file >> pixel[i].b; cout << (int)pixel[i].b << endl;
+		file >> temp; pixel[i].r = (float)temp;
+		file >> temp; pixel[i].g = (float)temp; 
+		file >> temp; pixel[i].b = (float)temp; 
 	}
 
+	delete[] pixel;
 	file.close();
 	return 0;
 }
